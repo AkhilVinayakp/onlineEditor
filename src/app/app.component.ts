4e3877391008575d59a4fc8eded0bc677b5bb5e3
Akhil Vinayak P <nirvanaosborn@gmail.com>
2019-06-09 14:13:38 +0530

    toggling side bar completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,38 @@ import { Component } from '@angular/core';
 export class AppComponent {
   toggle: boolean;
   title: string;
+  mc:number;
+  toggling_page={};
+  toggling_wrapper={}
+  toggling_sidebar={}
   constructor() {
-    this.toggle = false;
+    this.toggle = true;
     this.title = 'Online Editor';
+    this.mc=1;
+
   }
 
   toggleme(e) {
       console.log(e)
-     // $("#wrapper").toggleClass("toggled");
+      this.mc++;
+      if(this.mc % 2 ==0)
+            this.toggle=true;
+      else
+            this.toggle=false;
+      
+     this.toggling_page={
 
+       'page_ttogle':this.toggle
+     }
+     this.toggling_wrapper={
+        'ttoggle':this.toggle
+     }
+     this.toggling_sidebar =
+     {
+       'sidebar_ttoggle':this.toggle
+     }
+     this.toggle=false
   }
+ 
 }
 
